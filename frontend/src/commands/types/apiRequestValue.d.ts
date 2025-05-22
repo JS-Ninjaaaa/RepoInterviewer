7db1dfc2e09967eae95b7666ba01cf0de3ce4a76
Hikari <143927390+Hikaribamboo@users.noreply.github.com>
2025-05-22 10:38:38 +0900

    Update apiRequestValue.d.ts

--- frontend/src/commands/types/apiRequestValue.d.ts
+++ frontend/src/commands/types/apiRequestValue.d.ts
@@ -1,6 +1,6 @@
 export type apiRequestValue =
   | { type: 'fetchFirstQuestion'; payload: { difficulty: string; total_question: number } }
   | { type: 'fetchNextQuestion'; payload: { interview_id: string; question_id: number }}
-  | { type: 'fetchFeedBack'; payload: { interview_id: string; question_id: number, answer: string }}
+  | { type: 'fetchFeedback'; payload: { interview_id: string; question_id: number, answer: string }}
   | { type: 'fetchGeneralFeedback'; payload: { interview_id: string; }}
   | { type: 'closeWebview'; };
