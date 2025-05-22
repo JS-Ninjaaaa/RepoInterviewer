c54c584c270081d9a16c66ece47af37a83e52e8c
Hikaribamboo <hikari1653@gmail.com>
2025-05-22 23:11:57 +0900

    divided function of extension file

---
+++ frontend/src/commands/functions/utilities/getUri.ts
@@ -0,0 +1,11 @@
+import { Uri, Webview } from 'vscode';
+import * as vscode from "vscode";
+
+export function getUri(
+  webview: Webview, 
+  extensionUri: Uri, 
+  pathList: string[]
+): vscode.Uri {
+  return webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, ...pathList));
+}
+
