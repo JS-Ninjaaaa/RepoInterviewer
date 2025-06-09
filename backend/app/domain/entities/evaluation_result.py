7b33aea3b437e1d19708124691831978c861fc62
shunsei <k.shunsei1227@gmail.com>
2025-06-09 21:48:54 +0900

    Add getter & setter to entity

--- backend/app/domain/entities/evaluation_result.py
+++ backend/app/domain/entities/evaluation_result.py
@@ -1,7 +1,22 @@
 class QuestionEvaluationResult:
+    """質問の評価結果
+
+    Attributes:
+        score (int): 評価結果のスコア
+        comment (str): 評価結果のコメント
+    """
+
+    @property
+    def score(self) -> int:
+        return self._score
+
+    @property
+    def comment(self) -> str:
+        return self._comment
+
     def __init__(self, score: int, comment: str) -> None:
-        self.score = score
-        self.comment = comment
+        self._score = score
+        self._comment = comment
 
     @classmethod
     def from_dict(cls, data: dict) -> "QuestionEvaluationResult":
