35f1a914fd97f66037a7f8b4f330dfca5be12674
Hikaribamboo <hikari1653@gmail.com>
2025-06-10 16:13:04 +0900

    correct indent

--- frontend/src/commands/src/utilities/mappers.ts
+++ frontend/src/commands/src/utilities/mappers.ts
@@ -1,41 +1,51 @@
-import type { BackendFirstQuestionResponse, BackendNextQuestionResponse, BackendFeedBackResponse, BackendGeneralFeedbackResponse } from "@shared/backend-api-response-value";
-import type { FirstQuestion, NextQuestion, Feedback, GeneralFeedback } from "@shared/webview-api-response-type";
+import type {
+  BackendFirstQuestionResponse,
+  BackendNextQuestionResponse,
+  BackendFeedBackResponse,
+  BackendGeneralFeedbackResponse
+} from "@shared/backend-api-response-value";
+import type {
+  FirstQuestion,
+  NextQuestion,
+  Feedback,
+  GeneralFeedback
+} from "@shared/webview-api-response-type";
 
 // 変数名の変換関数の定義
 export function mapFirstQuestion(
-src: BackendFirstQuestionResponse
+  src: BackendFirstQuestionResponse
 ): FirstQuestion {
-return {
+  return {
     interviewId: src.interview_id,
-    question:     src.question
-};
+    question: src.question,
+  };
 }
 
 export function mapNextQuestion(
-src: BackendNextQuestionResponse
+  src: BackendNextQuestionResponse
 ): NextQuestion {
-return {
+  return {
     questionId: src.question_id,
-    question:   src.question
-};
+    question: src.question,
+  };
 }
 
 export function mapFeedback(
-src: BackendFeedBackResponse
+  src: BackendFeedBackResponse
 ): Feedback {
-return {
-    questionId:      src.question_id,
-    response:        src.response,
-    score:           src.score,
+  return {
+    questionId: src.question_id,
+    response: src.response,
+    score: src.score,
     continueQuestion: src.continue_question,
-};
+  };
 }
 
 export function mapGeneralFeedback(
-src: BackendGeneralFeedbackResponse
+  src: BackendGeneralFeedbackResponse
 ): GeneralFeedback {
-return {
-    scores:        src.scores,
-    generalReview: src.general_review
-};
+  return {
+    scores: src.scores,
+    generalReview: src.general_review,
+  };
 }
