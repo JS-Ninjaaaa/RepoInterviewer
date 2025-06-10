ad9375955bf4278a0728bc50e1a9e05de5774663
shunsei <k.shunsei1227@gmail.com>
2025-06-10 17:50:12 +0900

    Define get feedback usecase

--- backend/app/schemas/interview_schema.py
+++ backend/app/schemas/interview_schema.py
@@ -11,3 +11,16 @@ class SetUpInterviewRequest(BaseModel):
 class SetUpInterviewResponse(BaseModel):
     interview_id: str
     first_question: str
+
+
+class GetFeedbackRequest(BaseModel):
+    interview_id: str
+    question_id: str
+    message: str
+
+
+class GetFeedbackResponse(BaseModel):
+    interview_id: str
+    question_id: str
+    score: int
+    comment: str
