ccadfa5942d7b8ed848093a641a2eb942f347072
cercil0605 <kuamaichi2223@gmail.com>
2025-07-03 19:53:52 +0900

    add: auth token

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-from fastapi import Depends
-
+from fastapi import Depends,HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from app.domain.llm_clients.llm_client import LLMClient
 from app.domain.repositories.interview_repository import InterviewRepository
 from app.domain.repositories.source_code_repository import SourceCodeRepository
@@ -16,6 +16,9 @@ from app.usecase.usecases.get_interview_result_usecase import GetInterviewResult
 from app.usecase.usecases.get_question_usecase import GetQuestionUseCase
 from app.usecase.usecases.get_response_usecase import GetResponseUseCase
 from app.usecase.usecases.setup_interview_usecase import SetUpInterviewUseCase
+import os
+from app.infrastructure.llm_clients.token_auth import get_tokens_from_secret_manager
+
 
 
 def get_interview_repository() -> InterviewRepository:
@@ -72,3 +75,31 @@ def get_overall_review_usecase(
         interview_repository=interview_repository,
         llm_client=llm_client,
     )
+
+http_bearer = HTTPBearer(auto_error=False)
+
+def get_valid_tokens() -> str:
+    env = os.getenv("ENV", "prod").lower()
+    if env == "local":
+        return ""
+    return get_tokens_from_secret_manager()
+
+def verify_api_token(credentials: HTTPAuthorizationCredentials = Depends(http_bearer)):
+    tokens = get_valid_tokens()
+    # ローカルなら認証スキップ
+    if os.getenv("ENV", "prod").lower() == "local":
+        return
+    # トークンが存在しない場合
+    if credentials is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Authentication credentials were not provided.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    # トークンの認証
+    if credentials.credentials not in tokens:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid or expired token.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
